04b70172ff938525ead337d0c71734fe615ed214
bdrein <bdrein@bbcag.ch>
2022-06-02 10:42:05 +0200

    started with implementing search function. still at the beginning

--- app/src/main/java/ch/bbcag/lorlibrary/MainActivity.java
+++ app/src/main/java/ch/bbcag/lorlibrary/MainActivity.java
@@ -19,56 +19,58 @@ import ch.bbcag.lorlibrary.model.Card;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    System.out.println("test");
-    addCardsToClickableList();
-  }
-
-  // This method loads all the cards from the json file into Card Objects to then display
-  private void addCardsToClickableList() {
-
-    String string = "";
-    try {
-      InputStream inputStream = getAssets().open("set1-en_us.json");
-      int size = inputStream.available();
-      byte[] buffer = new byte[size];
-      inputStream.read(buffer);
-      string = new String(buffer);
-    } catch (IOException e) {
-      e.printStackTrace();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("test");
+        addCardsToClickableList();
     }
 
-    Gson gson = new Gson();
+    // This method loads all the cards from the json file into Card Objects to then display
+    private void addCardsToClickableList() {
 
-    Card[] cards = gson.fromJson(string, Card[].class);
-    ClickListener onClickListener =
-        position -> {
-          Intent intent = new Intent(this, CardImageTest.class);
-          Card selected = cards[position];
+        String string = "";
+        try {
+            InputStream inputStream = getAssets().open("set1-en_us.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            string = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-          intent.putExtra("cardCode", selected.getCardCode());
-          intent.putExtra("name", selected.getName());
-          intent.putExtra("cardImage", selected.getCardImage());
-          intent.putExtra("banner", selected.getBanner());
-          intent.putExtra("descriptionRaw", selected.getDescriptionRaw());
-          intent.putExtra("levelupDescriptionRaw", selected.getLevelupDescriptionRaw());
-          intent.putExtra("flavorText", selected.getFlavorText());
-          intent.putExtra("artistName", selected.getArtistName());
-          intent.putExtra("rarityRef", selected.getRarityRef());
-          intent.putExtra("type", selected.getType());
-          intent.putExtra("firstRegion", selected.getFirstRegion());
+        Gson gson = new Gson();
 
-          Bundle args = new Bundle();
-          startActivity(intent);
-        };
+        Card[] cards = gson.fromJson(string, Card[].class);
+        ClickListener onClickListener =
+                position -> {
+                    Intent intent = new Intent(this, CardImageTest.class);
+                    Card selected = cards[position];
 
-    RecyclerView recyclerView = findViewById(R.id.recycle_view);
-    RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this, cards, onClickListener);
-    RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
-    recyclerView.setLayoutManager(linearLayoutManager);
-    recyclerView.setAdapter(recyclerViewAdapter);
-  }
+                    intent.putExtra("cardCode", selected.getCardCode());
+                    intent.putExtra("name", selected.getName());
+                    intent.putExtra("cardImage", selected.getCardImage());
+                    intent.putExtra("banner", selected.getBanner());
+                    intent.putExtra("descriptionRaw", selected.getDescriptionRaw());
+                    intent.putExtra("levelupDescriptionRaw", selected.getLevelupDescriptionRaw());
+                    intent.putExtra("flavorText", selected.getFlavorText());
+                    intent.putExtra("artistName", selected.getArtistName());
+                    intent.putExtra("rarityRef", selected.getRarityRef());
+                    intent.putExtra("type", selected.getType());
+                    intent.putExtra("firstRegion", selected.getFirstRegion());
+
+                    Bundle args = new Bundle();
+                    startActivity(intent);
+                };
+
+        RecyclerView recyclerView = findViewById(R.id.recycle_view);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this, cards, onClickListener);
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+
 }
--- app/src/main/java/ch/bbcag/lorlibrary/RecyclerViewAdapter.java
+++ app/src/main/java/ch/bbcag/lorlibrary/RecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,75 +13,78 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import ch.bbcag.lorlibrary.model.Card;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
-  private Card[] cards;
-  private Context context;
-  private ClickListener clickListener;
+    private Card[] cards;
+    private Context context;
+    private ClickListener clickListener;
 
-  public RecyclerViewAdapter(Context context, Card[] cards, ClickListener clickListener) {
-    this.context = context;
-    this.cards = cards;
-    this.clickListener = clickListener;
-    System.out.println(Arrays.toString(cards));
-  }
-
-  @NonNull
-  @Override
-  public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(
-      @NonNull ViewGroup parent, int viewType) {
-    View view =
-        LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
-    return new MyViewHolder(view);
-  }
-
-  @Override
-  public void onBindViewHolder(
-      @NonNull RecyclerViewAdapter.MyViewHolder holder, final int position) {
-    final Card card = cards[position];
-    holder.title.setText(card.getName());
-
-    new DownloadImageFromInternet(holder.image.getContext(), holder.image)
-        .execute(card.getCardImage());
-    holder.cardView.setOnClickListener(
-        data -> {
-          Toast.makeText(context, "This is position of your Every ItemViews", Toast.LENGTH_SHORT)
-              .show();
-          clickListener.onItemClick(position);
-        });
-  }
-
-  @Override
-  public int getItemCount() {
-    return cards.length;
-  }
-
-  public class MyViewHolder extends RecyclerView.ViewHolder {
-    private TextView title;
-    private ImageView image;
-    private CardView cardView;
-
-    public MyViewHolder(@NonNull View itemView) {
-      super(itemView);
-      title = itemView.findViewById(R.id.card_name);
-      image = itemView.findViewById(R.id.card_image);
-      cardView = itemView.findViewById(R.id.card_view);
+    public RecyclerViewAdapter(Context context, Card[] cards, ClickListener clickListener) {
+        this.context = context;
+        this.cards = cards;
+        this.clickListener = clickListener;
+        System.out.println(Arrays.toString(cards));
     }
 
-    public TextView getTitle() {
-      return title;
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent, int viewType) {
+        View view =
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+        return new MyViewHolder(view);
     }
 
-    public ImageView getImage() {
-      return image;
+    @Override
+    public void onBindViewHolder(
+            @NonNull RecyclerViewAdapter.MyViewHolder holder, final int position) {
+        final Card card = cards[position];
+        holder.title.setText(card.getName());
+
+        new DownloadImageFromInternet(holder.image.getContext(), holder.image)
+                .execute(card.getCardImage());
+        holder.cardView.setOnClickListener(
+                data -> {
+                    Toast.makeText(context, "This is position of your Every ItemViews", Toast.LENGTH_SHORT)
+                            .show();
+                    clickListener.onItemClick(position);
+                });
     }
 
-    public CardView getCardView() {
-      return cardView;
+    @Override
+    public int getItemCount() {
+        return cards.length;
     }
-  }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+        private ImageView image;
+        private CardView cardView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.card_name);
+            image = itemView.findViewById(R.id.card_image);
+            cardView = itemView.findViewById(R.id.card_view);
+        }
+
+        public TextView getTitle() {
+            return title;
+        }
+
+        public ImageView getImage() {
+            return image;
+        }
+
+        public CardView getCardView() {
+            return cardView;
+        }
+    }
 }
+
